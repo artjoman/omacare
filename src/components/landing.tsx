7e9af665b009774f50040a98a0867caead1539c3
Peteris Arajs <peteris.arajs@gmail.com>
2020-11-07 11:50:15 +0200

    Update

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -26,6 +26,111 @@ const Logo = styled.img`
   object-position: center;
 `;
 
+const ButtonDark = styled.button`
+  left: 0px;
+  right: 0px;
+  top: 0px;
+  bottom: 0px;
+  background: #f31d4b;
+  border-radius: 4px;
+  height: 57px;
+  width: 259px;
+  left: 0px;
+  top: 0px;
+  border-radius: 4px;
+  border: #F2F2F2;
+  outline: none !important;
+  box-shadow: none;
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 16px;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.75px;
+  color: #F2F2F2;
+`;
+
+
+
+const ButtonLight = styled.button`
+  left: 0px;
+  right: 0px;
+  top: 0px;
+  bottom: 0px;
+  background: #ffeaea;
+  border-radius: 4px;
+  border: #EB5757;
+  height: 57px;
+  width: 259px;
+  left: 0px;
+  top: 0px;
+  outline: none !important;
+  box-shadow: none; 
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 16px;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.75px;
+  color: #EB5757;
+`;
+
+const ServiceDark = styled.button`
+  left: 0px;
+  right: 0px;
+  top: 0px;
+  bottom: 0px;
+  width: 340px;
+  height: 75px;
+  left: 18px;
+  top: 151px;
+  background: #FFEAEA;
+  border-radius: 60px;
+  border: #EB5757;
+  outline: none !important;
+  box-shadow: none; 
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 16px;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.75px;
+  color: #EB5757;
+`;
+
+
+const ServiceLight = styled.button`
+  left: 0px;
+  right: 0px;
+  top: 0px;
+  bottom: 0px;
+  width: 340px;
+  height: 75px;
+  left: 18px;
+  top: 331px;
+  background: #FFFFFF;
+  border: 4px solid #EB5757;
+  box-sizing: border-box;
+  border-radius: 60px;
+  outline: none !important;
+  box-shadow: none; 
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 16px;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.75px;
+  color: #EB5757;
+`;
+
 interface PropsFromState {
   loading: boolean;
   data: Oma[];
@@ -51,6 +156,19 @@ const Landing: React.FC<AllProps> = ({
   return (
     <Container>
       <Logo src="/images/logo.png" />
+
+    <Container>
+      <ButtonDark>I need help</ButtonDark>
+    </Container>
+    <Container>
+      <ButtonLight>I can help</ButtonLight>
+    </Container>
+    <Container>
+      <ServiceLight>I can help</ServiceLight>
+    </Container>
+    <Container>
+      <ServiceDark>I can help</ServiceDark>
+    </Container>
     </Container>
   );
 };
