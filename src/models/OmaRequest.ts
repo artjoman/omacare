faa323efb69b45f90b11996617cd326981a62c3a
Antti Rastas <antti.rastas@gmail.com>
2020-11-08 00:37:11 +0200

    Add title and image back to OmaRequest

--- src/models/OmaRequest.ts
+++ src/models/OmaRequest.ts
@@ -3,10 +3,12 @@ import { AgeGroup } from './AgeGroup';
 import { Service } from './Service';
 
 export default interface OmaRequest {
+  title: string;
   maxPrice: number;
   minPrice: number;
   description: string;
   services: Service[];
   gender: Gender;
   ageGroup: AgeGroup;
+  image: string;
 }
