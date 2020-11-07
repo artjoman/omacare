0a12e04e2ccf4ed02fff79d5f7c4e62997092456
Antti Rastas <antti.rastas@gmail.com>
2020-11-07 16:50:29 +0200

    Add MainButton component

---
+++ src/components/MainButton.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import React from "react";
+
+const styleMain = {
+  backgroundColor: "#F31D4B",
+  color: "#FFFFFF"
+}
+
+const styleMainDisabled = {
+  backgroundColor: "#F1F1F1",
+  color: "#4F4F4F"
+}
+
+interface Props {
+  labelText: string;
+  clickHandler: () => void;
+  enabled: boolean;
+}
+
+const MainButton: React.FC<Props> = ({ labelText, clickHandler, enabled }) => {
+  return (
+    <button style={enabled ? styleMain : styleMainDisabled} onClick={clickHandler}>{labelText}</button>
+  );
+};
+
+export default MainButton;
