36cfbee78f8ab19ed9903bf8f0ca4024f755c34b
Antti Rastas <antti.rastas@gmail.com>
2020-11-07 16:30:00 +0200

    Resolve conflicts in routes file

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,14 +4,6 @@ import { Route, Switch } from "react-router-dom";
 import Menu from "./components/menu";
 import Landing from "./components/landing";
 import Omas from "./components/omaList";
-<<<<<<< Updated upstream
-=======
-import Carer from "./components/carerList";
-import OmaDetails from "./components/omaDetails";
-import CarerDetails from "./components/carerDetails";
-import Splash from "./components/splash";
-import RequestForm from "./components/RequestForm";
->>>>>>> Stashed changes
 
 const Routes: React.FC = () => (
   <div>
